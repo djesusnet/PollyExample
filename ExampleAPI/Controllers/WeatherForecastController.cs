f21e2db3173d9f568b70bbcc372d2696c0802eed
djesu <djesu@JESUS-PC>
2024-06-02 21:09:09 -0300

    Add project files.

---
+++ ExampleAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExampleAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private static int callCount = 0;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+
+            callCount++;
+
+            if (callCount <= 3)
+            {
+                throw new HttpRequestException("Erro ao acessar a API");
+            
+            }
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ ExampleLibrayPolly/Program.cs
@@ -0,0 +1,51 @@
+﻿using Polly;
+using Polly.CircuitBreaker;
+
+var circuitBreakerPolicy = Policy
+    .Handle<HttpRequestException>()
+    .CircuitBreakerAsync(2, TimeSpan.FromSeconds(30));
+
+var retryPolicy = Policy
+    .Handle<HttpRequestException>()
+    .WaitAndRetryAsync(
+        retryCount: 3,
+        sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+        onRetry: (exception, timeSpan, retryCount, context) =>
+        {
+
+            Console.WriteLine($"Tentativa {retryCount} falhou. Tentando novamente em {timeSpan.Seconds} segundos.");
+        });
+
+
+
+var httpClient = new HttpClient();
+
+for (int i = 0; i < 5; i++)
+{
+    try
+    {
+        await circuitBreakerPolicy.ExecuteAsync(async () =>
+        {
+            var response = await httpClient.GetAsync("https://localhost:7247/WeatherForecast");
+            response.EnsureSuccessStatusCode();
+            Console.WriteLine("Requisição foi bem sucedida");
+        });
+
+        await retryPolicy.ExecuteAsync(async () =>
+        {
+            var response = await httpClient.GetAsync("https://localhost:7247/WeatherForecast");
+            response.EnsureSuccessStatusCode();
+            Console.WriteLine("Requisição foi bem sucedida");
+        });
+    }
+    catch (BrokenCircuitException ex)
+    {
+        Console.WriteLine($"Circuito está aberto. Falha detectada: {ex.Message}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Falha de requisição: {ex.Message}");
+    }
+
+    //await Task.Delay(5000);
+}
